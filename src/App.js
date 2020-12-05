e0239603df1882b31e675d57534f7380032332f4
Abdul-Razak <arazak4r@gmail.com>
2020-12-05 21:57:19 +0100

    frontend connected to commercejs backend. products added to commercejs backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { commerce } from "./Backend/Commerce";
 import { NavBar, Products } from "./components";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <NavBar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,72 +3,7 @@ import { Card, Container, Row, Col } from "react-bootstrap";
 
 import { BiCartAlt } from "react-icons/bi";
 
-const Products = (props) => {
-  const products = [
-    {
-      id: 1,
-      name: "Brown Brim",
-      imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
-      price: 25,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 2,
-      name: "Blue Beanie",
-      imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
-      price: 18,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 3,
-      name: "Brown Cowboy",
-      imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
-      price: 35,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 4,
-      name: "Grey Brim",
-      imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
-      price: 25,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 5,
-      name: "Green Beanie",
-      imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-      price: 18,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 6,
-      name: "Palm Tree Cap",
-      imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-      price: 14,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 7,
-      name: "Red Beanie",
-      imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-      price: 18,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 8,
-      name: "Wolf Cap",
-      imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-      price: 14,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-    {
-      id: 9,
-      name: "Blue Snapback",
-      imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
-      price: 16,
-      description: ";slkfadaa;'fadk'dfka'dfadakl'dfka",
-    },
-  ];
+const Products = ({ products }) => {
   return (
     <Container fluid>
       <Row>
@@ -77,17 +12,19 @@ const Products = (props) => {
             <Card key={product.id} style={{ width: "22rem", height: "25rem" }}>
               <Card.Img
                 variant="top"
-                src={product.imageUrl}
+                src={product.media.source}
                 style={{ width: "22rem", height: "18rem" }}
               />
               <Card.Body>
                 <Container>
                   <Row>
                     <Col>{product.name}</Col>
-                    <Col>{product.price}</Col>
+                    <Col>{product.price.formatted_with_symbol}</Col>
                   </Row>
                   <Row>
-                    <Col>{product.description}</Col>
+                    <Col
+                      dangerouslySetInnerHTML={{ __html: product.description }}
+                    />
                   </Row>
                   <Row>
                     <Col>
